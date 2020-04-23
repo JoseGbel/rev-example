3a5afcf80b8df4321424df64aa300b415211fc08
Jose Garcia <jose.gbel@gmail.com>
2020-04-23 13:49:52 +0100

    change brancgh

--- app/src/main/java/com/example/revoluttest/ui/main/RatesFragment.kt
+++ app/src/main/java/com/example/revoluttest/ui/main/RatesFragment.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.work.WorkerParameters
+import com.example.revoluttest.DataFetchingWorker
 import com.example.revoluttest.R
 import com.example.revoluttest.model.Currency
 import com.example.revoluttest.model.Rates
@@ -36,6 +38,8 @@ class RatesFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(RatesViewModel::class.java)
         viewModel.init("EUR")
+
+//        DataFetchingWorker.periodicWork()
 
         val ratesLiveData = viewModel.getRatesRepository()
 
